ea4712272da1bb617e89ed22c2cf48a31d8e1489
hieptq2 <hieptq11@gmail.com>
2020-03-11 14:05:34 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    //
+    //always open accordion on showcase block
+    $('#Accordion-Showcase [data-toggle="collapse"]').on('click', function (e) {
+        e.preventDefault();
+        if (!$(this).hasClass('collapsed')) {
+            e.stopPropagation();
+            return false;
+        }
+    });
 })
